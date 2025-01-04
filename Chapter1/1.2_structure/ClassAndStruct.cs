1f4d041ea9a2182404a85f1f21301c4c493f0727
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-04 18:38:11 +0900

    1.2 構造体 コード追加

---
+++ Chapter1/1.2_structure/ClassAndStruct.cs
@@ -0,0 +1,11 @@
+// クラス
+class MyClass {
+  public int X { get; set; }
+  public int Y { get; set; }
+}
+
+// 構造体
+struct MyStruct {
+  public int X { get; set; }
+  public int Y { get; set; }
+}
--- Chapter1/1.2_structure/Program.cs
+++ Chapter1/1.2_structure/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DateTime date = new(2025, 1, 4);
+int year = date.Year;
+// 10日後を求める
+DateTime daysAfter10 = date.AddDays(10);
+Console.WriteLine(daysAfter10);
+
+MyClass myClass = new MyClass { X = 1, Y = 2 };
+Console.WriteLine($"MyClass: X = {myClass.X}, Y = {myClass.Y}");
+
+MyStruct myStruct = new MyStruct { X = 3, Y = 4 };
+Console.WriteLine($"MyStruct: X = {myStruct.X}, Y = {myStruct.Y}");
+
+/*
+  クラスの場合、変数とは別の場所のヒープ領域が確保され、変数にはその参照が格納されるが、
+  構造体の場合は、変数そのものにオブジェクトが格納される。
+ */
