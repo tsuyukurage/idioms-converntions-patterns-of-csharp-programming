8feee83d50c1bd6f61cd27c91ed8d2e59f3c97bf
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-04 17:23:10 +0900

    1.1 クラス

--- 1.1_class/Program.cs
+++ 1.1_class/Program.cs
@@ -1,1 +1,9 @@
-Product karinto = new Product(123, "かりんとう", 180);
+﻿Product karinto = new(123, "かりんとう", 180);
+int price = karinto.Price;
+int taxIncluded = karinto.GetPriceIncludingTax();
+
+// 1つのクラスから複数のインスタンスを作成できる。
+Product daifuku = new(253, "大福もち", 160);
+
+int karintoTax = karinto.GetTax();
+int daifukuTax = daifuku.GetTax();
