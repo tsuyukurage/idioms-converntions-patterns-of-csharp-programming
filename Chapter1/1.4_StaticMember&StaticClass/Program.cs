9b08eb8c348d78f57e70af5a0e7852da2a03e88c
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-08 23:58:56 +0900

    静的クラス 追加

--- Chapter1/1.4_StaticMember&StaticClass/Program.cs
+++ Chapter1/1.4_StaticMember&StaticClass/Program.cs
@@ -1,3 +1,8 @@
 ﻿// newせずに使える
 DateTime today = DateTime.Today;
 Console.WriteLine($"今日は{today.Month}月{today.Day}日です");
+
+public static class Console {
+  // ビルドエラーになる
+  // Console con = new Console();
+}
