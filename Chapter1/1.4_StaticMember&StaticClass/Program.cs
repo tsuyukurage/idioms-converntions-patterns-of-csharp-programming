5e7248c9151fe475d86ffa069bace3d9f2daf656
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-08 23:45:49 +0900

    静的プロパティと静的メソッド

--- Chapter1/1.4_StaticMember&StaticClass/Program.cs
+++ Chapter1/1.4_StaticMember&StaticClass/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// newせずに使える
+DateTime today = DateTime.Today;
+Console.WriteLine($"今日は{today.Month}月{today.Day}日です");
