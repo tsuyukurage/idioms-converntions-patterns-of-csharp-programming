1921abd37aab3500831d5a538946f6a827bf6202
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-11 22:27:37 +0900

    1.6 継承 コード追加

---
+++ Chapter1/1.6_Inheritance/Employee.cs
@@ -0,0 +1,4 @@
+public class Employee : Person {
+  public int Id { get; set; }
+  public string DivisionName { get; set; }
+}
--- Chapter1/1.6_Inheritance/Program.cs
+++ Chapter1/1.6_Inheritance/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Employee employee = new Employee {
+  Id = 100,
+  Name = "山田太郎",
+  Birthday = new DateTime(1992, 4, 5),
+  DivisionName = "第一営業部",
+};
+Console.WriteLine($"{employee.Name}({employee.GetAge()})は、{employee.DivisionName}に所属しています。");
+
+Person person = new Employee(); // is-a関係
+// Employee employee2 = new Person(); // エラー not is-a関係
+
+// クラス階層の頂点にあるのがSystem.Objectクラス
+object person2 = new Person();
+object employee2 = new Employee();
