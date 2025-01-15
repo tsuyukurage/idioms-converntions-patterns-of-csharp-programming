a49019ec4125846780d3989c54447e1c0289d44d
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-15 22:59:15 +0900

    Chapter1 演習 追加

---
+++ Chapter1/Exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿// question1.1
+Product dorayaki = new(98, "どら焼き", 210);
+int taxIncluded = dorayaki.GetPriceIncludingTax();
+
+Console.WriteLine(taxIncluded);
+
+// question1.2
+
+// question1.3
+// question1.4
