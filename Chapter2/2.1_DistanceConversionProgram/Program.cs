4ac0d3d23f2aac66221a5f5f5d109385e245d552
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-18 23:59:00 +0900

    メソッドを独立

--- Chapter2/2.1_DistanceConversionProgram/Program.cs
+++ Chapter2/2.1_DistanceConversionProgram/Program.cs
@@ -1,12 +1,20 @@
 ﻿if (args.Length >= 1 && args[0] == "-tom") {
-  // フィートからメートルへの対応表を出力
-  for (int feet = 1; feet <= 10; feet++) {
+  PrintFeetToMeterList(1, 10);
+} else {
+  PrintMeterToFeetList(1, 10);
+}
+
+// フィートからメートルへの対応表を出力
+static void PrintFeetToMeterList(int start, int stop) {
+  for (int feet = start; feet <= stop; feet++) {
     double meter = FeetToMeter(feet);
     Console.WriteLine($"{feet}ft = {meter:N4}m");
   }
-} else {
-  // メートルからフィートへの対応表を出力
-  for (int meter = 1; meter <= 10; meter++) {
+}
+
+// メートルからフィートへの対応表を出力
+PrintMeterToFeetList(int start, int stop) {
+  for (int meter = start; meter <= stop; meter++) {
     double feet = MeterToFeet(meter);
     Console.WriteLine($"{meter}m = {feet:N4}ft");
   }
