32be3a420c87910fa52dac55a67d42faa53833a7
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-20 22:44:43 +0900

    クラスを利用

--- Chapter2/2.1_DistanceConversionProgram/Program.cs
+++ Chapter2/2.1_DistanceConversionProgram/Program.cs
@@ -1,4 +1,6 @@
-﻿if (args.Length >= 1 && args[0] == "-tom") {
+﻿using DistanceConverter;
+
+if (args.Length >= 1 && args[0] == "-tom") {
   PrintFeetToMeterList(1, 10);
 } else {
   PrintMeterToFeetList(1, 10);
@@ -6,16 +8,18 @@
 
 // フィートからメートルへの対応表を出力
 static void PrintFeetToMeterList(int start, int stop) {
+  FeetConverter converter = new();
   for (int feet = start; feet <= stop; feet++) {
-    double meter = FeetToMeter(feet);
+    double meter = converter.ToMeter(feet);
     Console.WriteLine($"{feet}ft = {meter:N4}m");
   }
 }
 
 // メートルからフィートへの対応表を出力
-PrintMeterToFeetList(int start, int stop) {
+static void PrintMeterToFeetList(int start, int stop) {
+  FeetConverter converter = new();
   for (int meter = start; meter <= stop; meter++) {
-    double feet = MeterToFeet(meter);
+    double feet = converter.FromMeter(meter);
     Console.WriteLine($"{meter}m = {feet:N4}ft");
   }
 }
