20453dbd80d41833a7d5510ba9d55d99ec46ae07
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-22 23:07:01 +0900

    定数を定義

--- Chapter2/2.1_DistanceConversionProgram/FeetConverter.cs
+++ Chapter2/2.1_DistanceConversionProgram/FeetConverter.cs
@@ -1,14 +1,16 @@
 namespace DistanceConverter;
 
 public static class FeetConverter {
+  private const double ratio = 0.3048;
+
   // メートルからフィートを求める
   public static double FromMeter(double meter) {
-    return meter / 0.3048;
+    return meter / ratio;
   }
 
   // フィートからメートルを求める
   public static double ToMeter(double feet) {
-    return feet * 0.3048;
+    return feet * ratio;
   }
 
 }
