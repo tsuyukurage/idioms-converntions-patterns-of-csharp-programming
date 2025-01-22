41b77d7e2fa42ef11bebb852c1451cd9cefa7e33
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-22 23:03:02 +0900

    静的メソッドに変更

--- Chapter2/2.1_DistanceConversionProgram/FeetConverter.cs
+++ Chapter2/2.1_DistanceConversionProgram/FeetConverter.cs
@@ -2,12 +2,12 @@ namespace DistanceConverter;
 
 public class FeetConverter {
   // メートルからフィートを求める
-  public double FromMeter(double meter) {
+  public static double FromMeter(double meter) {
     return meter / 0.3048;
   }
 
   // フィートからメートルを求める
-  public double ToMeter(double feet) {
+  public static double ToMeter(double feet) {
     return feet * 0.3048;
   }
 
--- Chapter2/2.1_DistanceConversionProgram/Program.cs
+++ Chapter2/2.1_DistanceConversionProgram/Program.cs
@@ -8,18 +8,16 @@ if (args.Length >= 1 && args[0] == "-tom") {
 
 // フィートからメートルへの対応表を出力
 static void PrintFeetToMeterList(int start, int stop) {
-  FeetConverter converter = new();
   for (int feet = start; feet <= stop; feet++) {
-    double meter = converter.ToMeter(feet);
+    double meter = FeetConverter.ToMeter(feet);
     Console.WriteLine($"{feet}ft = {meter:N4}m");
   }
 }
 
 // メートルからフィートへの対応表を出力
 static void PrintMeterToFeetList(int start, int stop) {
-  FeetConverter converter = new();
   for (int meter = start; meter <= stop; meter++) {
-    double feet = converter.FromMeter(meter);
+    double feet = FeetConverter.FromMeter(meter);
     Console.WriteLine($"{meter}m = {feet:N4}ft");
   }
 }
