942cef73343981c31693ddcf2020502649cead8d
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-26 23:59:00 +0900

    CSVファイルを読み込み

--- Chapter2/2.2_SalesCalculator/Program.cs
+++ Chapter2/2.2_SalesCalculator/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// 売り上げデータを読み込み、Saleオブジェクトのリストを返す
+static List<Sale> ReadSales(string filePath) {
+  List<Sale> sales = new List<Sale>();
+  string[] lines = File.ReadAllLines(filePath);
+  foreach(string line in lines) {
+    string[] items = line.Split(',');
+    sales sale = new Sale {
+      ShopName = items[0],
+      ProductCategory = items[1],
+      Amount = int.Parse(items[2]),
+    };
+    sales.Add(sale);
+  }
+  return sales;
+}
