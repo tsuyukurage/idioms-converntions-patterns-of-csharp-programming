a52341c695059dea0f9364bb34e7daad5216e3e3
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-27 23:52:16 +0900

    SalesCounterクラス定義

---
+++ Chapter2/2.2_SalesCalculator/SalesCounter.cs
@@ -0,0 +1,10 @@
+// 売り上げ集計クラス
+public class SalesCounter {
+
+  private readonly List<SalesCounter> _sales;
+
+  // コンストラクタ
+  public SalesCounter(List<SalesCounter> sale) {
+    _sales = sale;
+  }
+}
