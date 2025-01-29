fe161dc434b6ebe83251b3ce867e177b2dadde9a
tsuyuri-n <t.suzuki.9657@gmail.com>
2025-01-29 23:59:00 +0900

    GetPerStoreSalesメソッド定義

--- Chapter2/2.2_SalesCalculator/SalesCounter.cs
+++ Chapter2/2.2_SalesCalculator/SalesCounter.cs
@@ -7,4 +7,16 @@ public class SalesCounter {
   public SalesCounter(List<SalesCounter> sale) {
     _sales = sale;
   }
+
+  // 店舗売り上げを求める
+  public Dictionary<string, int> GetPerStoreSales() {
+    Dictionary<string, int> dict = new Dictionary<string, int>();
+    foreach (SalesCounter sale in _sales) {
+      if (dict.ContainsKey(sale.ShopName))
+        dict[sale.ShopName] += sale.Amount;
+      else
+        dict[sale.ShopName] = sale.Amount;
+    }
+    return dict;
+  }
 }
